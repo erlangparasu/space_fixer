674dd8d5f93751b2022db55a94f62dc56bd49579
Erlang Parasu <erlangparasu@gmail.com>
2022-01-29 01:16:50 +0800

    example1

---
+++ examples/example1.dart
@@ -0,0 +1,50 @@
+/// ErlangParasu 2022
+///
+library space_fixer;
+
+import 'package:flutter/material.dart';
+import 'package:space_fixer/space_fixer.dart';
+
+Widget widgetExample1(BuildContext context) {
+  return Container(
+    color: Colors.blue,
+    child: ListView(
+      physics: const BouncingScrollPhysics(
+        parent: AlwaysScrollableScrollPhysics(),
+      ),
+      children: [
+        Container(),
+        SpaceFixerHorizontalLine(
+          context: context,
+          overflowHeight: 3,
+          overflowColor: Colors.black,
+        ),
+        Container(),
+        SpaceFixerHorizontalLine(
+          context: context,
+          overflowHeight: 3,
+          overflowColor: Colors.black,
+        ),
+        Container(),
+        SpaceFixerHorizontalLine(
+          context: context,
+          overflowHeight: 3,
+          overflowColor: Colors.black,
+        ),
+        Container(),
+        SpaceFixerHorizontalLine(
+          context: context,
+          overflowHeight: 3,
+          overflowColor: Colors.black,
+        ),
+        Container(),
+        SpaceFixerHorizontalLine(
+          context: context,
+          overflowHeight: 3,
+          overflowColor: Colors.black,
+        ),
+        Container(),
+      ],
+    ),
+  );
+}
