c23768e243b504341832e710db0bf20d02f3e85c
Deepanshu Chaudhary <deepanshuc2141@outlook.com>
2022-08-08 04:39:31 +0530

    Deprecated context and now using layout builder to get constraints if width or height not provided.

--- lib/space_fixer.dart
+++ lib/space_fixer.dart
@@ -6,81 +6,101 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SpaceFixerHorizontalLine extends StatelessWidget {
-  SpaceFixerHorizontalLine({
-    Key? key,
-    required this.context,
+  const SpaceFixerHorizontalLine({
+    super.key,
+    @Deprecated('Context is no longer required.') this.context,
     required this.overflowColor,
     this.width,
     required this.overflowHeight,
-  }) : super(key: key) {
-    width ??= MediaQuery.of(context).size.width;
-  }
+    this.border,
+  });
 
-  BuildContext context;
-  Color overflowColor;
-  double? width;
-  double overflowHeight;
+  final BuildContext? context;
+  final Color overflowColor;
+  final double? width;
+  final double overflowHeight;
+  final BoxBorder? border;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      height: 0,
-      child: Stack(
-        clipBehavior: Clip.none,
-        alignment: AlignmentDirectional.center,
-        children: [
-          Positioned(
-            width: width,
-            height: overflowHeight,
-            child: Container(
-              width: width,
-              height: overflowHeight,
-              color: overflowColor,
-            ),
+    return LayoutBuilder(
+      builder: (BuildContext buildContext, BoxConstraints boxConstraints) {
+        final double width;
+        width = this.width ??
+            boxConstraints.maxWidth; //MediaQuery.of(context).size.width - 1;
+        return SizedBox(
+          width: width,
+          height: 0,
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: AlignmentDirectional.center,
+            children: [
+              Positioned(
+                width: width,
+                height: overflowHeight,
+                child: Container(
+                  width: width,
+                  height: overflowHeight,
+                  decoration: BoxDecoration(
+                    color: overflowColor,
+                    border: border,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
 
 class SpaceFixerVerticalLine extends StatelessWidget {
-  SpaceFixerVerticalLine({
-    Key? key,
-    required this.context,
+  const SpaceFixerVerticalLine({
+    super.key,
+    @Deprecated('Context is no longer required.') this.context,
     required this.overflowColor,
     this.overflowWidth = 2,
     this.height,
-  }) : super(key: key) {
-    height ??= MediaQuery.of(context).size.height;
-  }
+    this.border,
+  });
 
-  BuildContext context;
-  Color overflowColor;
-  double overflowWidth;
-  double? height;
+  final BuildContext? context;
+  final Color overflowColor;
+  final double overflowWidth;
+  final double? height;
+  final BoxBorder? border;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 0,
-      height: height,
-      child: Stack(
-        clipBehavior: Clip.none,
-        alignment: AlignmentDirectional.center,
-        children: [
-          Positioned(
-            width: overflowWidth,
-            height: height,
-            child: Container(
-              width: overflowWidth,
-              height: height,
-              color: overflowColor,
-            ),
+    return LayoutBuilder(
+      builder: (BuildContext buildContext, BoxConstraints boxConstraints) {
+        final double height;
+        height = this.height ??
+            boxConstraints.maxHeight; //MediaQuery.of(context).size.height;
+        return SizedBox(
+          width: 0,
+          height: height,
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: AlignmentDirectional.center,
+            children: [
+              Positioned(
+                width: overflowWidth,
+                height: height,
+                child: Container(
+                  width: overflowWidth,
+                  height: height,
+                  decoration: BoxDecoration(
+                    color: overflowColor,
+                    border: border,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- test/space_fixer_test.dart
+++ test/space_fixer_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:space_fixer/space_fixer.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
